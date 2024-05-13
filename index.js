f17cdcef63a3c46b2ceded71a9ce82dadc92a442
maab-fw <maabfw@proton.me>
2024-05-13 13:29:24 +0600

    jwt post created, and verify token middleware created

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const cors = require("cors")
+const jwt = require("jsonwebtoken")
+const cookieParser = require("cookie-parser")
 require("dotenv").config()
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb")
 const port = process.env.PORT || 5000
@@ -7,6 +9,8 @@ const port = process.env.PORT || 5000
 const app = express()
 
 app.use(express.json())
+app.use(cookieParser())
+
 app.use(
     cors({
         origin: ["http://localhost:5173", "https://maab-fw-assignment-11.vercel.app"],
@@ -26,6 +30,22 @@ const client = new MongoClient(uri, {
     },
 })
 
+const verifyToken = (req, res, next) => {
+    const token = req.cookies?.token
+    if (!token) return res.status(401).send({ message: "Unauthorized access" })
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) return res.status(401).send({ message: "Unauthorized access" })
+        req.user = decoded
+        next()
+    })
+}
+
+const cookieOptions = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -33,6 +53,19 @@ async function run() {
 
         const servicesCollection = client.db("serviceFlowDB").collection("services")
         const bookingsCollection = client.db("serviceFlowDB").collection("bookings")
+
+        app.post("/jwt", async (req, res) => {
+            const user = req.body
+            // console.log("user for token", user)
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "365d" })
+
+            res.cookie("token", token, cookieOptions).send({ success: true })
+        })
+
+        app.post("/logout", async (req, res) => {
+            const user = req.body
+            res.clearCookie("token", { ...cookieOptions, maxAge: 0 }).send({ success: true })
+        })
 
         app.post("/add-a-service", async (req, res) => {
             const serviceData = req.body
